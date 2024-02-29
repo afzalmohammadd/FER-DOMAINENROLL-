cdc747f33b0676b36750e06f49a4457e13840800
afzalmohammadd <121129798+afzalmohammadd@users.noreply.github.com>
2024-02-29 14:20:52 +0530

    update origin

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://precious-malabi-008175.netlify.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
